f26e95b1f8e199788368d6237a2e7ca66ace4878
Dark Spectra <163740044+mr-mrz-1@users.noreply.github.com>
2024-10-22 22:44:43 +0530

    Update ResumeUtils.java
    update for implementing in ourr project

--- ResumeUtils.java
+++ ResumeUtils.java
@@ -3,20 +3,17 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.sql.Connection;
-import java.sql.DriverManager;
+// import java.sql.Connection;
+// import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class ResumeUtils {
-   public static   String URL = "jdbc:mysql://localhost:3306/jobportal?useSSL=false";
-     public static  String username = "root"; 
-     public  static String password = "735403"; 
-
+  
    
-    public static void uploadResume() {
+    public static void uploadResume(int id) {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Select Resume to Upload");
         fileChooser.setFileFilter(new FileNameExtensionFilter("PDF Files", "pdf"));
@@ -27,15 +24,15 @@ public class ResumeUtils {
             File selectedFile = fileChooser.getSelectedFile();
             String insertSql = "INSERT INTO pdf_files (email, resume_pdf) VALUES (?, ?)";
 
-            try (Connection connection = DriverManager.getConnection(URL,username,password);
-                 PreparedStatement pstmt = connection.prepareStatement(insertSql)) {
+            try (
+                 PreparedStatement pstmt = jdbc.con.prepareStatement(insertSql)) {
 
                 InputStream inputStream = new FileInputStream(selectedFile);
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Enter your email: ");
-                String email = sc.nextLine();
+                // Scanner sc = new Scanner(System.in);
+                // System.out.print("Enter your email: ");
+                // String email = sc.nextLine();
 
-                pstmt.setString(1, email);
+                pstmt.setInt(1, id);
                 pstmt.setBlob(2, inputStream);                  
                 pstmt.executeUpdate();
                 System.out.println("Resume uploaded successfully to the database.");
